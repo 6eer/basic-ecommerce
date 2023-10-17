1d4f30526010837f33a4d85d8e0cb34fcf9ff499
FacundoFalchini <facundofaclchini@gmail.com>
2023-10-17 11:20:34 -0300

    add docker and docker compose

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,7 @@ console.log(process.env.DB_NAME);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_ENGINE,
+  process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
